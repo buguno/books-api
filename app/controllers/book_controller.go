f3500f8effe8fdf8e61df624a3af68d6e9d27421
buguno <brunnodesouzabezerra@gmail.com>
2023-03-21 00:49:56 -0300

    ✨ feat: creating get books controller

---
+++ app/controllers/book_controller.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// GetBooks func gets all exists books.
+// @Description Get all exists books.
+// @Summary get all exists books
+// @Tags Books
+// @Accept json
+// @Produce json
+// @Success 200 {array} models.Book
+// @Router /v1/books [get]
+func GetBooks(c *fiber.Ctx) error {
+	// Create database connection.
+	db, err := database.OpenDBConnection()
+	if err != nil {
+		// Return status 500 and database connection error.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Get all books.
+	books, err := db.GetBooks()
+	if err != nil {
+		// Return, if books not found.
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error":   true,
+			"message": "books were not found",
+			"count":   0,
+			"books":   nil,
+		})
+	}
+
+	// Return status 200 OK.
+	return c.JSON(fiber.Map{
+		"error":   false,
+		"message": nil,
+		"count":   len(books),
+		"books":   books,
+	})
+}
